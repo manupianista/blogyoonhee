0cb6cf58e9d13d5a424fb1c9f84c8d477fdfcb47
manupianista <manuelecastilloo@gmail.com>
2019-05-24 11:43:16 -0600

    cositas

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
@@ -10,7 +10,11 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <?php wp_head(); ?>
+    <?php wp_head(); 
+    //get_template_part('partials/navbarmenutop');
+    //get_header('navbarmenu');
+    
+    ?>
 </head>
 
 <body <?php body_class(); ?>>
