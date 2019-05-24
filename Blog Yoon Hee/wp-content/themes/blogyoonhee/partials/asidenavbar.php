416d6504260be5cec8a8b1a31aaefe309d0fee34
manupianista <manuelecastilloo@gmail.com>
2019-05-24 13:09:36 -0600

    template para arrancar

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/partials/asidenavbar.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/partials/asidenavbar.php
@@ -1,26 +1,45 @@
-<div id="colorlib-page">
-		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
-		<aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
-			<h1 id="colorlib-logo"><a href="index.html"><span class="img" style="background-image: url(images/author.jpg);"></span>Yoon Hee</a></h1>
-			<nav id="colorlib-main-menu" role="navigation">
-				<ul>
-					<li class="colorlib-active"><a href="index.html">HOME</a></li>
-					<li><a href="collection.html">GALLERY</a></li>
-					<li><a href="about.html">COLLECTION NO. 1</a></li>
-					<li><a href="services.html">BEST COLLECTION</a></li>
-					<li><a href="contact.html">Contact</a></li>
-				</ul>
-			</nav>
-
-			<div class="colorlib-footer">
-				<h3>Newsletter</h3>
-				<div class="d-flex justify-content-center">
-					<form action="#" class="colorlib-subscribe-form">
-            <div class="form-group d-flex">
-            	<div class="icon"><span class="icon-paper-plane"></span></div>
-              <input type="text" class="form-control" placeholder="Enter Email Address">
+<!-- Sidebar  -->
+<nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Yoon Hee</h3>
             </div>
-          </form>
-				</div>
-			</div>
-		</aside> <!-- END COLORLIB-ASIDE -->
+
+            <ul class="list-unstyled components">
+                
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="">HOME</a>
+                    <!--<ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">HOME</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 3</a>
+                        </li>
+                    </ul>-->
+                </li>
+                <li>
+                    <a href="#">GALLERY</a>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">COLLECTIONS</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">COLLECTION NO. 1</a>
+                        </li>
+                        <li>
+                            <a href="#">BEST PROYECT</a>
+                        </li>
+                        
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">CONTACT</a>
+                </li>
+                
+            </ul>
+
+          
+        </nav>
