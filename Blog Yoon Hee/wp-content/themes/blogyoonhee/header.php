a63f0908a9ec5fadb030701daedff77ade0023bd
manupianista <manuelecastilloo@gmail.com>
2019-05-26 13:47:40 -0600

    arreglos css y js

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
@@ -10,17 +10,25 @@
 
      <!-- Scrollbar Custom CSS -->
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+     
+
+     <!-- Notre CSS -->
+     <link rel="stylesheet" href="style.css">
  
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <?php wp_head(); 
-    get_template_part('partials/asidenavbar');
+    
     //get_header('navbarmenu');
     
     ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body 
+<?php 
+body_class(); 
+get_template_part('partials/asidenavbar');
+?>
 
