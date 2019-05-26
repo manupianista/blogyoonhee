60f2996481d767dfb0bf6b1bada63c85734d98b6
manupianista <manuelecastilloo@gmail.com>
2019-05-26 13:27:24 -0600

    cambios minimos para todos xdxd

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
@@ -8,25 +8,9 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Herr+Von+Muellerhoff" rel="stylesheet">
 
-    <link rel="stylesheet" href="assets/css/custom/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/custom/animate.css">
-    
-    <link rel="stylesheet" href="assets/css/custom/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/custom/owl.theme.default.min.css">
-    <link rel="stylesheet" href="assets/css/custom/magnific-popup.css">
-
-    <link rel="stylesheet" href="assets/css/custom/aos.css">
-
-    <link rel="stylesheet" href="assets/css/custom/ionicons.min.css">
-
-    <link rel="stylesheet" href="assets/css/custom/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="assets/css/custom/jquery.timepicker.css">
-
-    
-    <link rel="stylesheet" href="assets/css/custom/flaticon.css">
-    <link rel="stylesheet" href="assets/css/custom/icomoon.css">
-    <link rel="stylesheet" href="assets/css/custom/style.css">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+     <!-- Scrollbar Custom CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+ 
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
--- Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/home-template.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/home-template.php
@@ -8,12 +8,39 @@ Template Name: home-template
 
 <?php get_header(); ?>
 
-<div class="wrapper">
         
 
         <!-- Page Content  -->
         <div id="content">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
 
+                        <button type="button" id="sidebarCollapse" class="btn btn-info">
+                            <i class="fas fa-align-left"></i>
+                            <span>Toggle Sidebar</span>
+                        </button>
+                        <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-align-justify"></i>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="nav navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Page</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Page</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Page</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Page</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             
 
             <h2>Collapsible Sidebar</h2>
@@ -36,5 +63,28 @@ Template Name: home-template
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
+
+
+     <!-- jQuery Custom Scroller CDN -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+   
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#sidebar").mCustomScrollbar({
+                theme: "minimal"
+            });
+
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar, #content').toggleClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            });
+        });
+    </script>
 
 <?php get_footer(); ?>
