659de9a03f0f1be6bcbe0aa5c517d4fbdc8f80cc
manupianista <manuelecastilloo@gmail.com>
2019-05-26 14:24:32 -0600

    varios

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/header.php
@@ -12,8 +12,7 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
      
 
-     <!-- Notre CSS -->
-     <link rel="stylesheet" href="style.css">
+   
  
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
