300e23a1a78351b1fadec038749ba8ed291df343
Kevin Champney <kevinchampney@hotmail.com>
2019-05-26 13:54:13 -0600

    Add collection template

---
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/collection-template.php
@@ -0,0 +1,77 @@
+<?php 
+
+/*
+Template Name: Collections 1 - template
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="container gallery-container">
+  
+  <h1 class="text-center">Bootstrap 3 Gallery</h1>
+
+  <p class="page-description text-center">Grid Layout With Zoom Effect</p>
+    
+  <div class="tz-gallery">
+
+      <div class="row mb-3">
+          <div class="col-md-4">
+              <div class="card">
+                  <a class="lightbox">
+                  <img src="http://localhost/blogyoonhee/Blog%20Yoon%20Hee/wp-content/uploads/2019/05/DSF9993.jpg" alt="Park" class="card-img-top">
+                  </a>
+              </div>
+          </div>
+           
+          <div class="col-md-4">
+              <div class="card">
+                  <a class="lightbox">
+                  <img src="http://localhost/blogyoonhee/Blog%20Yoon%20Hee/wp-content/uploads/2019/05/DSF9990.jpg" alt="Park" class="card-img-top">
+                  </a>
+              </div>
+          </div>
+           
+          <div class="col-md-4">
+              <div class="card">
+                  <a class="lightbox">
+                  <img src="http://localhost/blogyoonhee/Blog%20Yoon%20Hee/wp-content/uploads/2019/05/DSF9967.jpg" alt="Park" class="card-img-top">
+                  </a>
+              </div>
+          </div>
+      </div>
+        <div class="row"> 
+          <div class="col-md-4">
+              <div class="card">
+                  <a class="lightbox">
+                  <img src="images/coast.jpg" alt="Park" class="card-img-top">
+                  </a>
+              </div>
+          </div>
+           
+          <div class="col-md-4">
+              <div class="card">
+                  <a class="lightbox">
+                  <img src="images/rails.jpg" alt="Park" class="card-img-top">
+                  </a>
+              </div>
+          </div>
+           
+          <div class="col-md-4">
+              <div class="card">
+                  <a class="lightbox">
+                  <img src="images/rocks.jpg" alt="Park" class="card-img-top">
+                  </a>
+              </div>
+          </div>
+       
+      </div>
+
+  </div>
+
+</div>
+
+</div>
+
+<?php get_footer(); ?>
