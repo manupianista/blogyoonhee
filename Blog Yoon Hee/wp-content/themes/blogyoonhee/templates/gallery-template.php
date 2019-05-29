5b78ce19f5c95517eb417e3e845c1bd7228144d1
Kevin Champney <kevinchampney@hotmail.com>
2019-05-29 00:33:13 -0600

    Responsive fixed

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/gallery-template.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/gallery-template.php
@@ -15,71 +15,44 @@ get_template_part('partials/navbars/forgallery');
 <div id="content">
     <?php get_template_part('partials/toggle'); ?>
             
-<!-- Gallery Content -->
-<section class="gallery-block cards-gallery">
-   	<div class="container">
-   		<div class="row">
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF0003.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-1.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   			
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF0013.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-2.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   			
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9966.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-3.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   		</div>
-   		
-   		
-   		<div class="row">
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9978.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-4.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   			
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9990.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-5.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   			
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF0003.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-1.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   		</div>
-
-		<div class="row">
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-				   	<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF0013.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-2.jpg" class="card-img-top"></a>
+	<div class="galerias">
+		<!-- Gallery Content -->
+		<section class="gallery-block cards-gallery">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
+						<div class="card border-0 transform-on-hover">
+							<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF0003.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-1.jpg" class="card-img-top"></a>
+						</div>
+					</div>
+					
+					<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
+						<div class="card border-0 transform-on-hover">
+							<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF0013.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-2.jpg" class="card-img-top"></a>
+						</div>
+					</div>
+					
+					<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
+						<div class="card border-0 transform-on-hover">
+							<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9966.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-3.jpg" class="card-img-top"></a>
+						</div>
+					</div>
+					
+					<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
+						<div class="card border-0 transform-on-hover">
+							<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9978.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-4.jpg" class="card-img-top"></a>
+						</div>
+					</div>
+					
+					<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
+						<div class="card border-0 transform-on-hover">
+							<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9990.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-5.jpg" class="card-img-top"></a>
+						</div>
+					</div>
 				</div>
-   			</div>
-   			
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9966.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-3.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   			
-   			<div class="col-md-6 col-lg-4 pl-2 pr-2  py-3">
-   				<div class="card border-0 transform-on-hover">
-   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/DSF9978.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH-Gallery-4.jpg" class="card-img-top"></a>
-   				</div>
-   			</div>
-   		</div>
-   	</div>
-   </section>
+			</div>
+		</section>
+	</div>
 </div>
 
 <!-- baguetteBox JS -->       
