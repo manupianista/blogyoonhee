0c3db3f07e6bf24512232fbed74d68bb06f5544b
Kevin Champney <kevinchampney@hotmail.com>
2019-05-28 22:56:27 -0600

    Best project page

--- Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/best-project-template.php
+++ Blog Yoon Hee/wp-content/themes/blogyoonhee/templates/best-project-template.php
@@ -7,20 +7,64 @@ Template Name: bestprojects-template
 ?>
 
 <?php 
-get_header(); 
-get_template_part('partials/navbars/forbest');
+get_header();
+get_template_part('partials/navbars/forcoll1');
 ?>
 
-        
+<!-- Page Content  -->
+<div id="content">
+    <?php get_template_part('partials/toggle'); ?>
+
+<div class="galerias">
+<!-- Collection No. 1 Content -->
+<section class="gallery-block cards-gallery">
+   	<div class="container">
+   		<div class="row">
+   			<div class="col-md-6 col-lg-4 pl-2 pr-2 py-3">
+   				<div class="card border-0 transform-on-hover">
+   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH1_resized.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH1_resized.jpg" class="card-img-top"></a>
+   				</div>
+   			</div>
+   			
+   			<div class="col-md-6 col-lg-4 pl-2 pr-2 py-3">
+   				<div class="card border-0 transform-on-hover">
+   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH2_resized.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH2_resized.jpg" class="card-img-top"></a>
+   				</div>
+   			</div>
+   			
+   			<div class="col-md-6 col-lg-4 pl-2 pr-2 py-3">
+   				<div class="card border-0 transform-on-hover">
+   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH3_resized.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH3_resized.jpg" class="card-img-top"></a>
+   				</div>
+   			</div>
+   		</div>
+   		
+   		
+   		<div class="row">
+   			<div class="col-md-6 col-lg-4 pl-2 pr-2 py-3">
+   				<div class="card border-0 transform-on-hover">
+   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH4_resized.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH4_resized.jpg" class="card-img-top"></a>
+   				</div>
+   			</div>
+   			
+   			<div class="col-md-6 col-lg-4 pl-2 pr-2 py-3">
+   				<div class="card border-0 transform-on-hover">
+   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH5_resized.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH5_resized.jpg" class="card-img-top"></a>
+   				</div>
+   			</div>
+   			
+   			<div class="col-md-6 col-lg-4 pl-2 pr-2 py-3">
+   				<div class="card border-0 transform-on-hover">
+   					<a class="lightbox" href="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH6_resized.jpg"><img src="http://www.yoonheefashion.info/wp-content/uploads/2019/05/PH6_resized.jpg" class="card-img-top"></a>
+   				</div>
+   			</div>
+   		</div>
+   	</div>
+   </section>
+</div>
+</div>
+
+<!-- baguetteBox JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
     
-        <!-- Page Content  -->
-        <div id="content">
-
-            <?php get_template_part('partials/toggle'); ?>
-            
-        </div>
-    </div>
-
-
-     
 <?php get_footer(); ?>
